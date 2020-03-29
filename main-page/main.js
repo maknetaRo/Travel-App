68933343269ec3defb4a679ce5f9cc142d6b81ad
sara90 <sbr.90@hotmail.com>
2020-03-29 12:23:58 +0200

    added changes in user account

--- main-page/main.js
+++ main-page/main.js
@@ -10,6 +10,12 @@ const uList = document.getElementById("todo-list");
 const placeCity = document.getElementById("city-name");
 const mainPage = document.getElementById("main-page");
 const userAccount = document.getElementById("user-account");
+const hotelBtn = document.getElementById('hotels-btn');
+const restaurantBtn = document.getElementById('restaurants-btn');
+const thingstodoBtn = document.getElementById('thingstodo-btn');
+const monumentsBtn = document.getElementById('monuments-btn');
+const showInfo = document.getElementById('show-info');
+
 
 /*placeInput.addEventListener("keyup", function(e) {
   const placeName = e.target.value;
@@ -56,12 +62,68 @@ function eventListeners() {
     document.getElementById("depart-form").reset();*/
     e.preventDefault();
     placeCity.innerText = placeInput.value;
-    placeInput.value = "";
     mainPage.classList.add("hide");
     userAccount.classList.remove("hide");
     document.body.style.backgroundImage = "url(./img/travel.jpg)";
   });
+
+  thingstodoBtn.addEventListener('click', () => {
+    if (showInfo.classList.contains('hide')) {
+      showInfo.classList.remove('hide')
+      showInfo.innerHTML = `
+      <iframe id="thingstodo-frame"
+      width="1200"
+      height="1000"
+      src="https://foursquare.com/explore?cat=arts&mode=url&near=${placeInput.value}">
+      </iframe>`
+    } else {
+      showInfo.classList.add('hide')
+    }
+  });
+
+  restaurantBtn.addEventListener('click', () => {
+    if (showInfo.classList.contains('hide')) {
+      showInfo.classList.remove('hide')
+      showInfo.innerHTML = `
+      <iframe id="restaurants-frame"
+      width="1200"
+      height="1000"
+      src="https://foursquare.com/explore?cat=food&mode=url&near=${placeInput.value}">
+    </iframe>`
+    } else {
+      showInfo.classList.add('hide')
+    }
+  });
+
+  monumentsBtn.addEventListener('click', () => {
+    if (showInfo.classList.contains('hide')) {
+      showInfo.classList.remove('hide')
+      showInfo.innerHTML = `
+      <iframe id="monuments-frame"
+      width="1200"
+      height="1000"
+      src="https://foursquare.com/explore?mode=url&near=${placeInput.value}&q=Monument">
+    </iframe>`
+    } else {
+      showInfo.classList.add('hide')
+    }
+  })
+
+  hotelBtn.addEventListener('click', () => {
+    if (showInfo.classList.contains('hide')) {
+      showInfo.classList.remove('hide')    
+      showInfo.innerHTML = `
+      <iframe id="hotels-frame"
+      width="1200"
+      height="1000"
+      src="https://foursquare.com/explore?mode=url&near=${placeInput.value}&q=Hotel">
+    </iframe>`
+    } else {
+      showInfo.classList.add('hide')
+    }
+  })
 }
+
 
 function startFunction() {
   let inputBarValue = inputBar.value;
