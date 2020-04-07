f8b2d3af686d852d7fc60f0abd03a05b5f9f3be9
maknetaRo <makneta@gmail.com>
2020-04-08 00:25:40 +0200

    feat: Add weather api

--- main-page/main.js
+++ main-page/main.js
@@ -1,7 +1,8 @@
 const submitBtn = document.getElementById("submit");
-const placeInput = document.forms["place-form"].querySelector("input");
-const arriveInput = document.forms["arrive-form"].querySelector("input");
-const departInput = document.forms["depart-form"].querySelector("input");
+const inputs = document.getElementById("main-form").elements;
+const placeInput = inputs["place"];
+const arriveInput = inputs["trip-start"];
+const departInput = inputs["trip-end"];
 const inputBar = document.getElementById("todo-input-bar");
 const addBtn = document.getElementById("add-todo-btn");
 const uList = document.getElementById("todo-list");
@@ -16,24 +17,15 @@ const showInfo = document.getElementById("show-info");
 const cityBoxesBtn = Array.from(document.getElementsByClassName("box"));
 const closeBar = document.getElementById("close-bar");
 const showInfoDiv = document.getElementById("showInfoDiv");
+const firstDay = document.getElementById("initial-weather");
+const finalDay = document.getElementById("final-weather");
 
-/*placeInput.addEventListener("keyup", function(e) {
-  const placeName = e.target.value;
-  console.log(placeName);
-  return placeName;
-});
-arriveInput.addEventListener("input", function(e) {
-  const arriveDate = e.target.value;
-  console.log(arriveDate);
-  return arriveDate;
-});
-departInput.addEventListener("input", function(e) {
-  const departDate = e.target.value;
-  console.log(departDate);
-  return departDate;
-});*/
+const api = {
+  key: "bc5968f4b239943f185627ac83619ed6",
+  baseurl: "https://api.openweathermap.org/data/2.5/",
+};
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   eventListeners();
 });
 
@@ -42,13 +34,13 @@ function eventListeners() {
     startFunction();
   });
 
-  inputBar.addEventListener("keypress", e => {
+  inputBar.addEventListener("keypress", (e) => {
     if (e.key === "Enter") {
       startFunction();
     }
   });
 
-  uList.addEventListener("click", function(event) {
+  uList.addEventListener("click", function (event) {
     if (event.target.classList.contains("done-btn")) {
       tickFromList(event.target);
     } else if (event.target.classList.contains("delete-btn")) {
@@ -56,19 +48,93 @@ function eventListeners() {
     }
   });
 
-  submitBtn.addEventListener("click", function(e) {
-    /*document.getElementById("place-form").reset();
-    document.getElementById("arrive-form").reset();
-    document.getElementById("depart-form").reset();*/
+  submitBtn.addEventListener("click", function (e) {
     e.preventDefault();
     placeCity.innerText = placeInput.value;
     mainPage.classList.add("hide");
     userAccount.classList.remove("hide");
     document.body.style.backgroundImage = "url(./img/travel.jpg)";
+
+    getResults(placeInput.value, arriveInput.value, departInput.value);
+    resetForm();
   });
 
-  cityBoxesBtn.forEach(box =>
-    box.addEventListener("click", e => {
+  //  weather forecast api
+
+  function getResults(query) {
+    fetch(`${api.baseurl}forecast?q=${query}&units=metric&APPID=${api.key}`)
+      .then((forecast) => {
+        return forecast.json();
+      })
+      .then(displayForecast);
+  }
+  function displayForecast(forecast) {
+    let city = placeInput.value;
+    let arr = forecast.list;
+    // first day
+    let obj = arr.find((o) => o.dt_txt.includes("15:00:00"));
+    // date
+    const initialDate = document.createElement("P");
+    initialDate.innerText = arriveInput.value;
+    firstDay.appendChild(initialDate);
+
+    // icon
+    let firstIconCode = obj.weather[0].icon;
+    let firstDayWeatherDescription = obj.weather[0].description;
+    const img = document.createElement("IMG");
+    img.src = "http://openweathermap.org/img/w/" + firstIconCode + ".png";
+    img.alt = firstDayWeatherDescription;
+    firstDay.appendChild(img);
+
+    // temperature
+    const initialTemp = document.createElement("H3");
+    let firstDayTemperature = `${Math.round(obj.main.temp)} C`;
+    initialTemp.innerText = firstDayTemperature;
+    firstDay.appendChild(initialTemp);
+
+    // weather description
+    const firstDayWeather = document.createElement("H4");
+    firstDayWeather.innerText = firstDayWeatherDescription;
+    firstDay.appendChild(firstDayWeather);
+
+    // last day
+    let listOfDays = arr.filter((elem) => elem.dt_txt.includes("15:00:00"));
+    let lastDay = listOfDays.length - 1;
+
+    // date
+    const lastDate = document.createElement("P");
+    lastDate.innerText = departInput.value;
+    finalDay.appendChild(lastDate);
+
+    // icon
+    let lastIconCode = listOfDays[lastDay].weather[0].icon;
+    let lastDayWeatherDescription = listOfDays[lastDay].weather[0].description;
+    const finalDayImg = document.createElement("IMG");
+    finalDayImg.src =
+      "http://openweathermap.org/img/w/" + lastIconCode + ".png";
+    console.log(lastIconCode);
+    finalDayImg.alt = lastDayWeatherDescription;
+    finalDay.appendChild(finalDayImg);
+
+    // temperature
+    let finalTemp = document.createElement("H3");
+    const lastDayTemp = `${Math.round(listOfDays[lastDay].main.temp)} C`;
+    console.log(lastDayTemp);
+    finalTemp.innerText = lastDayTemp;
+    finalDay.appendChild(finalTemp);
+
+    // description
+    let lastDayWeather = document.createElement("H4");
+    lastDayWeather.innerText = lastDayWeatherDescription;
+    finalDay.append(lastDayWeather);
+  }
+
+  function resetForm() {
+    inputs.reset();
+  }
+
+  cityBoxesBtn.forEach((box) =>
+    box.addEventListener("click", (e) => {
       showInfoDiv.classList.remove("hide");
 
       if (e.target.value === "thingstodo") {
