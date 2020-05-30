4cd31931d5ef12263a9024d8b1dd92f474f0c5a0
maknetaRo <makneta@gmail.com>
2020-05-31 01:11:01 +0200

    Add registartion form

--- django-app/account/views.py
+++ django-app/account/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 from django.contrib.auth.decorators import login_required
 
 
-# def register(request):
-#     return render(request, "account/register.html")
+def register(request):
+    if request.method == "POST":
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # set the chosen password
+            new_user.set_password(user_form.cleaned_data["password"])
+            # Save the user object
+            new_user.save()
+            return render(request, "account/register_done.html", {"new_user": new_user})
+    else:
+        user_form = UserRegistrationForm
+    return render(request, "account/register.html", {"user_form": user_form})
 
 
 # def login(request):
